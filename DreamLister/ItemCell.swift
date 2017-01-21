db86ff247e814b21fb0acb8e8da48865b00ffd00
ATrueNovice <sean.haughtonscott@gmail.com>
2017-01-20 20:08:40 -0500

    Added ItemCell for data manipulation, connected tableview to ItemCellVC, added tableView methods to count, and created the outlets for change

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,19 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by New on 1/20/17.
+//  Copyright © 2017 HSI. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+
+
+}
--- DreamLister/MainVC.swift
+++ DreamLister/MainVC.swift
@@ -8,16 +8,38 @@
 
 import UIKit
 
-class MainVC: UIViewController {
+class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+
+    //Outlets from the ViewController
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var segment: UISegmentedControl!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        //The tableview will receive and delegate information from this VC
+
+        tableView.delegate = self
+        tableView.dataSource = self
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    //Methods that will be used in the table view
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+        
     }
 
 
